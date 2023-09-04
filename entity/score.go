862c74f480d59eced949249c57c8d54f1f7f11ae
Porping <porsteam@hotmail.com>
2023-09-04 18:21:56 +0700

    [ADDED] #2: added lecture id in to score

--- entity/score.go
+++ entity/score.go
@@ -3,9 +3,11 @@ package entity
 type Score struct {
 	ID           string  `json:"id" gorm:"primaryKey;type:char(255)"`
 	Score        float64 ` json:"score"`
-	StudentId    string  `json:"student_id"`
+	StudentID    string  `json:"student_id"`
+	LecturerID   string  `json:"lecturer_id"`
 	AssessmentID string  `json:"assessment_id"`
 
 	Student    Student
+	Lecturer   Lecturer
 	Assessment Assessment
 }
