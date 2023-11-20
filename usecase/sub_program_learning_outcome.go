031c9b8fecbcd8c45b63ea401ee2c57f93756922
XiaoXuxxxx <73125291+XiaoXuxxxx@users.noreply.github.com>
2023-11-20 13:23:59 +0000

    feat: make repo, usecase, controller of sub plo handling error more properly

--- usecase/sub_program_learning_outcome.go
+++ usecase/sub_program_learning_outcome.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"github.com/oklog/ulid/v2"
 	"github.com/team-inu/inu-backyard/entity"
+	errs "github.com/team-inu/inu-backyard/entity/error"
 )
 
 type subSubProgramLearningOutcomeUsecase struct {
@@ -14,11 +15,21 @@ func NewSubProgramLearningOutcomeUsecase(subSubProgramLearningOutcomeRepo entity
 }
 
 func (c subSubProgramLearningOutcomeUsecase) GetAll() ([]entity.SubProgramLearningOutcome, error) {
-	return c.subSubProgramLearningOutcomeRepo.GetAll()
+	splos, err := c.subSubProgramLearningOutcomeRepo.GetAll()
+	if err != nil {
+		return nil, errs.New(errs.ErrQuerySubPLO, "cannot get all sub plos", err)
+	}
+
+	return splos, nil
 }
 
 func (c subSubProgramLearningOutcomeUsecase) GetByID(id string) (*entity.SubProgramLearningOutcome, error) {
-	return c.subSubProgramLearningOutcomeRepo.GetByID(id)
+	splo, err := c.subSubProgramLearningOutcomeRepo.GetByID(id)
+	if err != nil {
+		return nil, errs.New(errs.ErrQuerySubPLO, "cannot get sub plo by id %s", id, err)
+	}
+
+	return splo, nil
 }
 
 func (c subSubProgramLearningOutcomeUsecase) Create(code string, descriptionThai string, descriptionEng string, programLearningOutcomeId string) (*entity.SubProgramLearningOutcome, error) {
@@ -33,12 +44,17 @@ func (c subSubProgramLearningOutcomeUsecase) Create(code string, descriptionThai
 	err := c.subSubProgramLearningOutcomeRepo.Create(&splo)
 
 	if err != nil {
-		return nil, err
+		return nil, errs.New(errs.ErrCreateSubPLO, "cannot create sub plo", err)
 	}
 
 	return &splo, nil
 }
 
 func (c subSubProgramLearningOutcomeUsecase) Delete(id string) error {
-	return c.subSubProgramLearningOutcomeRepo.Delete(id)
+	err := c.subSubProgramLearningOutcomeRepo.Delete(id)
+	if err != nil {
+		return errs.New(errs.ErrDeleteSubPLO, "cannot delete sub plo", err)
+	}
+
+	return nil
 }
