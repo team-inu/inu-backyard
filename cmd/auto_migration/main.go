b3cd26f7b0c4bece0ed1b92f03d0720d503080ad
XiaoXuxxxx <73125291+XiaoXuxxxx@users.noreply.github.com>
2024-02-21 00:05:10 +0700

    feat: add script for auto generate tables
    Co-authored-by: Porping <porsteam@hotmail.com>
    Co-authored-by: Nathee Jaywaree <eltfshr@users.noreply.github.com>

---
+++ cmd/auto_migration/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/team-inu/inu-backyard/entity"
+	"github.com/team-inu/inu-backyard/infrastructure/database"
+)
+
+func main() {
+	// for development purpose only
+	gormDB, err := database.NewGorm(&database.GormConfig{
+		User:         "root",
+		Password:     "root",
+		Host:         "localhost",
+		DatabaseName: "inu_backyard",
+		Port:         "3306",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = gormDB.AutoMigrate(
+		&entity.Assignment{},
+		&entity.CourseLearningOutcome{},
+		&entity.Course{},
+		&entity.Department{},
+		&entity.Enrollment{},
+		&entity.Faculty{},
+		&entity.Grade{},
+		&entity.GraduatedStudent{},
+		&entity.Lecturer{},
+		&entity.ProgramLearningOutcome{},
+		&entity.ProgramOutcome{},
+		&entity.Programme{},
+		&entity.Score{},
+		&entity.Semester{},
+		&entity.Session{},
+		&entity.Student{},
+		&entity.SubProgramLearningOutcome{},
+	)
+
+	fmt.Println(err)
+}
