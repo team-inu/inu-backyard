6ad0a940807153775f985d99b001f54b8f571eff
Porping <porsteam@hotmail.com>
2024-02-23 22:02:54 +0700

    feat: add more course grade

--- entity/course.go
+++ entity/course.go
@@ -1,16 +1,18 @@
 package entity
 
 type CriteriaGrade struct {
-	A float64 `json:"criteriaGradeA" gorm:"column:criteria_grade_a" validate:"required"`
-	B float64 `json:"criteriaGradeB" gorm:"column:criteria_grade_b" validate:"required"`
-	C float64 `json:"criteriaGradeC" gorm:"column:criteria_grade_c" validate:"required"`
-	D float64 `json:"criteriaGradeD" gorm:"column:criteria_grade_d" validate:"required"`
-	E float64 `json:"criteriaGradeE" gorm:"column:criteria_grade_e" validate:"required"`
-	F float64 `json:"criteriaGradeF" gorm:"column:criteria_grade_f" validate:"required"`
+	A  float64 `json:"criteriaGradeA" gorm:"column:criteria_grade_a" validate:"required"`
+	BP float64 `json:"criteriaGradeBP" gorm:"column:criteria_grade_bp" validate:"required"`
+	B  float64 `json:"criteriaGradeB" gorm:"column:criteria_grade_b" validate:"required"`
+	CP float64 `json:"criteriaGradeCP" gorm:"column:criteria_grade_cp" validate:"required"`
+	C  float64 `json:"criteriaGradeC" gorm:"column:criteria_grade_c" validate:"required"`
+	DP float64 `json:"criteriaGradeDP" gorm:"column:criteria_grade_dp" validate:"required"`
+	D  float64 `json:"criteriaGradeD" gorm:"column:criteria_grade_d" validate:"required"`
+	F  float64 `json:"criteriaGradeF" gorm:"column:criteria_grade_f" validate:"required"`
 }
 
 func (c CriteriaGrade) IsValid() bool {
-	return c.A >= c.B && c.B >= c.C && c.C >= c.D && c.D >= c.E && c.E >= c.F
+	return c.A >= 0 && c.BP >= 0 && c.B >= 0 && c.CP >= 0 && c.C >= 0 && c.DP >= 0 && c.D >= 0 && c.F >= 0
 }
 
 type Course struct {
