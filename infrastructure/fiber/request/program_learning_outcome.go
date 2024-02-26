e03a7be34336aac6bb9e13b874a23ce70445c7f5
Nathee Jaywaree <puntf2@gmail.com>
2024-02-26 16:25:20 +0000

    feat: change descriptionEng to not be required

--- infrastructure/fiber/request/program_learning_outcome.go
+++ infrastructure/fiber/request/program_learning_outcome.go
@@ -1,9 +1,9 @@
 package request
 
 type CrateProgramLearningOutcome struct {
-	Code                       string                           `validate:"required"`
-	DescriptionThai            string                           `validate:"required"`
-	DescriptionEng             string                           `validate:"required"`
+	Code                       string `validate:"required"`
+	DescriptionThai            string `validate:"required"`
+	DescriptionEng             string
 	ProgramYear                int                              `validate:"required"`
 	ProgrammeName              string                           `validate:"required"`
 	SubProgramLearningOutcomes []CreateSubProgramLeaningOutcome `validate:"dive"`
