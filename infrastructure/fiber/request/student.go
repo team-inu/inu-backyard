1f06d12a99507f867b4bb0104fbc69abefa05b94
XiaoXuxxxx <73125291+XiaoXuxxxx@users.noreply.github.com>
2024-03-20 15:31:14 +0000

    fix: error when send gpax to zero
    Co-authored-by: Porping <Porping@users.noreply.github.com>
    Co-authored-by: Nathee Jaywaree <eltfshr@users.noreply.github.com>

--- infrastructure/fiber/request/student.go
+++ infrastructure/fiber/request/student.go
@@ -1,19 +1,19 @@
 package request
 
 type CreateStudentPayload struct {
-	KmuttId   string  `json:"kmuttId" validate:"required"`
-	FirstName string  `json:"firstName" validate:"required"`
-	LastName  string  `json:"lastName" validate:"required"`
-	GPAX      float64 `json:"gpax" validate:"required"`
-	MathGPA   float64 `json:"mathGPA" validate:"required"`
-	EngGPA    float64 `json:"engGPA" validate:"required"`
-	SciGPA    float64 `json:"sciGPA" validate:"required"`
-	School    string  `json:"school" validate:"required"`
-	City      string  `json:"city" validate:"required"`
-	Email     string  `json:"email" validate:"required"`
-	Year      string  `json:"year" validate:"required"`
-	Admission string  `json:"admission" validate:"required"`
-	Remark    string  `json:"remark"`
+	KmuttId   string   `json:"kmuttId" validate:"required"`
+	FirstName string   `json:"firstName" validate:"required"`
+	LastName  string   `json:"lastName" validate:"required"`
+	GPAX      *float64 `json:"gpax" validate:"required"`
+	MathGPA   *float64 `json:"mathGPA" validate:"required"`
+	EngGPA    *float64 `json:"engGPA" validate:"required"`
+	SciGPA    *float64 `json:"sciGPA" validate:"required"`
+	School    string   `json:"school" validate:"required"`
+	City      string   `json:"city" validate:"required"`
+	Email     string   `json:"email" validate:"required"`
+	Year      string   `json:"year" validate:"required"`
+	Admission string   `json:"admission" validate:"required"`
+	Remark    string   `json:"remark"`
 
 	ProgrammeName  string `json:"programmeName" validate:"required"`
 	DepartmentName string `json:"departmentName" validate:"required"`
@@ -30,19 +30,19 @@ type CreateBulkStudentsPayload struct {
 }
 
 type UpdateStudentPayload struct {
-	KmuttId   string  `json:"kmuttId" validate:"required"`
-	FirstName string  `json:"firstName" validate:"required"`
-	LastName  string  `json:"lastName" validate:"required"`
-	GPAX      float64 `json:"gpax" validate:"required"`
-	MathGPA   float64 `json:"mathGPA" validate:"required"`
-	EngGPA    float64 `json:"engGPA" validate:"required"`
-	SciGPA    float64 `json:"sciGPA" validate:"required"`
-	School    string  `json:"school" validate:"required"`
-	City      string  `json:"city" validate:"required"`
-	Email     string  `json:"email" validate:"required"`
-	Year      string  `json:"year" validate:"required"`
-	Admission string  `json:"admission" validate:"required"`
-	Remark    *string `json:"remark" validate:"required"`
+	KmuttId   string   `json:"kmuttId" validate:"required"`
+	FirstName string   `json:"firstName" validate:"required"`
+	LastName  string   `json:"lastName" validate:"required"`
+	GPAX      *float64 `json:"gpax" validate:"required"`
+	MathGPA   *float64 `json:"mathGPA" validate:"required"`
+	EngGPA    *float64 `json:"engGPA" validate:"required"`
+	SciGPA    *float64 `json:"sciGPA" validate:"required"`
+	School    string   `json:"school" validate:"required"`
+	City      string   `json:"city" validate:"required"`
+	Email     string   `json:"email" validate:"required"`
+	Year      string   `json:"year" validate:"required"`
+	Admission string   `json:"admission" validate:"required"`
+	Remark    *string  `json:"remark" validate:"required"`
 
 	ProgrammeName  string `json:"programmeName" validate:"required"`
 	DepartmentName string `json:"departmentName" validate:"required"`
